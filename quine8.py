d8031ff445bc0d1d1b86ea1e1cca2e1fd27d05af
Jesse Fish <jessefish@gmail.com>
2015-12-22 13:00:36 -0500

    initial commit

---
+++ quine8.py
@@ -0,0 +1,4 @@
+# Python program that prints its own code
+
+a = "b = chr(97) + chr(32) + chr(61) + chr(32) + chr(34); b += a; print b + chr(34); print a"
+b = chr(97) + chr(32) + chr(61) + chr(32) + chr(34); b += a; print b + chr(34); print a
